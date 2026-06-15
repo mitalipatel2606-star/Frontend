f8ac8b1d6034b174f22fab727e040448eefc56b2
mitalipatel2606-star <mitalipatel2606@gmail.com>
2026-06-15 18:33:09 +0530

    feat: implement state-managed counter with increment and decrement functionality in App component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  let counter = 5;
+
+  const [counter, setCounter] = useState(5)
+  //let counter = 5;
   const addValue = () => {
     console.log("value added ", counter);
-    counter = counter + 1;
+    if (counter < 25)
+      setCounter(counter + 1)
+  }
+  const removeValue = () => {
+    if (counter > 0)
+      setCounter(counter - 1)
   }
 
   return (
@@ -17,7 +24,8 @@ function App() {
       <h2>Counter Value:{counter}</h2>
       <button
         onClick={addValue}>Increase Counter:{counter}</button>
-      <button>Decrease Counter:{counter}</button>
+      <button
+        onClick={removeValue}>Decrease Counter:{counter}</button>
       <p>footer:{counter}</p>
     </>
   )
