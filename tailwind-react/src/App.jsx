1c917ee67dc51d78918e87f3a00ef5e051d643af
mitalipatel2606-star <mitalipatel2606@gmail.com>
2026-06-16 21:41:28 +0530

    chore: initialize project dependencies and node_modules structure

---
+++ tailwind-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-blue p-4 rounded' >Mitali Patel</h1>
+    </>
+  )
+}
+
+export default App
