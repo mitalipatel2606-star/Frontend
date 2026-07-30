d08eb8f9b1cbcfd02bd367f829979e7042764895
mitalipatel2606-star <mitalipatel2606@gmail.com>
2026-07-30 23:31:54 +0530

    feat: implement password generator logic and UI using useCallback hook

--- passGen/passGen/src/App.jsx
+++ passGen/passGen/src/App.jsx
@@ -1,14 +1,45 @@
 import { useState } from 'react'
+import { useCallback } from 'react';
+//useCallback is used to cache functions during re-rendering
 
 import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("")
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*()_~`"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+
+
 
   return (
     <>
-      <h1 className='text-4xl text-blue-500 text-center py-10'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 
+      text-orange-500 bg-gray-700'>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input
+            type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='password'
+            readOnly></input>
+        </div>
+      </div>
     </>
+
   )
 }
 
